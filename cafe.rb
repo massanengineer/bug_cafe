c5eb2a51ffc4fdf4d43c76e39a6b075a517959f3
massan <matsuda4645@gmail.com>
2022-03-31 10:16:37 +0900

    再修正しました。

--- cafe.rb
+++ cafe.rb
@@ -25,10 +25,10 @@ def take_order(menus)
 end
 
 puts 'bugカフェへようこそ！ご注文は？ 番号でどうぞ'
-order_DRINKS = take_order(DRINKS)
+order_drinks = take_order(DRINKS)
 
 puts 'フードメニューはいかがですか?'
-order_FOODS = take_order(FOODS)
+order_foods = take_order(FOODS)
 
-total = DRINKS[order1][:price] + FOODS[order2][:price]
+total = DRINKS[order_drinks][:price] + FOODS[order_foods][:price]
 puts "お会計は#{total}円になります。ありがとうございました！"
